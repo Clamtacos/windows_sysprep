ebe821cfd64a181f8e5d0fa6787a2000eaa4cdf6
clamtacos <5408736+Clamtacos@users.noreply.github.com>
2026-07-16 04:47:14 -0400

    Update windows_sysprep.ps1

--- windows_sysprep.ps1
+++ windows_sysprep.ps1
@@ -16,7 +16,7 @@ None
 Start-Transcript $ENV:TEMP\windows_sysprep.log
 
 Set-ExecutionPolicy Bypass -Force:$True -Confirm:$False -ErrorAction SilentlyContinue
-Set-Variable -Name 'ConfirmPreference' -Value 'None' -Scope Global
+# Set-Variable -Name 'ConfirmPreference' -Value 'None' -Scope Global
 
 $ProgressPreference = 'SilentlyContinue'
 $json = Get-Content "($PSScriptRoot)\windows_sysprep.json" | ConvertForm-Json
@@ -27,26 +27,28 @@ if (!(Get-AppxPackage -Name Microsoft.Winget.Source)) {
     Invoke-Webrequest -uri https://aka.ms/getwinget -Outfile $ENV:TEMP\winget.msixbundle -UseBasicParsing
     Add-AppxPackage $ENV:TEMP\Microsoft.VCLibs.x64.14.00.Desktop.appx -ErrorAction SilentlyContinue
     Add-AppxPackage -Path $ENV:TEMP\winget.msixbundle -ErrorAction SilentlyContinue
-}
 
-$CurrentVC = Get-WmiObject -Class Win32_Product -Filter "Name LIKE '%Visual C++%'" -ErrorAction SilentlyContinue | Select-Object Name
-Foreach ($App in $json.MicrosftVCRuntime) {
-Write-Host ("Verifying if {0} is installed..." -f $App)
-    if (!($CurrentVC | Select-String $App.split('+')[2].SubString(0, 4) | Select-String $App.split('-')[1])) {
-        Write-Host ("{0} not found, installing..." -f $App)
-        winget.exe install $App --force --source winget --accept-package-agreements --accept-source-agreements
-    }
-}
-
-Foreach ($App in $json.Apps) {
+    $CurrentVC = Get-WmiObject -Class Win32_Product -Filter "Name LIKE '%Visual C++%'" -ErrorAction SilentlyContinue | Select-Object Name
+        Foreach ($App in $json.MicrosftVCRuntime) {
+        Write-Host ("Verifying if {0} is installed..." -f $App)
+            if (!($CurrentVC | Select-String $App.split('+')[2].SubString(0, 4) | Select-String $App.split('-')[1])) {
+            Write-Host ("{0} not found, installing..." -f $App)
+            winget.exe install $App --force --source winget --accept-package-agreements --accept-source-agreements
+            }
+        }
+    Foreach ($App in $json.Apps) {
     Write-Host ("Verifying if {0} is installed..." -f $App)
     winget.exe list --id $App --accept-source-agreements | Out-Null
-    if ($LASTEXITCODE -eq '-1978335212') {
-        Write-Host ("{0} not found, installing..." -f $App.Split('.')[1])
-        winget.exe install $App --silent --force --source winget --accept-package-agreements --accept-source-agreements
-    } 
+        if ($LASTEXITCODE -eq '-1978335212') {
+            Write-Host ("{0} not found, installing..." -f $App.Split('.')[1])
+            winget.exe install $App --silent --force --source winget --accept-package-agreements --accept-source-agreements
+        } 
+    }
 }
-
-Set-PSRepository PSGallery -InstallationPolicy Trusted
-
 Stop-Transcript
+
+
+
+
+
+

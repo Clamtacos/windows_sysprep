33fe8ff1412c303d7c40860efc06ddbd4e7ac1b8
clamtacos <5408736+Clamtacos@users.noreply.github.com>
2026-07-17 04:50:27 -0400

    Update windows_sysprep.ps1

--- windows_sysprep.ps1
+++ windows_sysprep.ps1
@@ -15,7 +15,7 @@ None
 #Requires -RunAsAdministrator
 Start-Transcript $ENV:TEMP\windows_sysprep.log
 
-Set-ExecutionPolicy Bypass -Scope CurrentUser -Force:$True -Confirm:$False -ErrorAction SilentlyContinue
+Set-ExecutionPolicy Bypass -Scope Global -Force:$True -Confirm:$False -ErrorAction SilentlyContinue
 Set-Variable -Name 'ConfirmPreference' -Value 'None' -Scope Global
 
 $ProgressPreference = 'SilentlyContinue'
@@ -27,15 +27,15 @@ if (!(Get-AppxPackage -Name Microsoft.Winget.Source)) {
     Invoke-Webrequest -uri https://aka.ms/getwinget -Outfile $ENV:TEMP\winget.msixbundle -UseBasicParsing
     Add-AppxPackage $ENV:TEMP\Microsoft.VCLibs.x64.14.00.Desktop.appx -ErrorAction SilentlyContinue
     Add-AppxPackage -Path $ENV:TEMP\winget.msixbundle -ErrorAction SilentlyContinue
-
+    }
     $CurrentVC = Get-WmiObject -Class Win32_Product -Filter "Name LIKE '%Visual C++%'" -ErrorAction SilentlyContinue | Select-Object Name
-        Foreach ($App in $json.MicrosftVCRuntime) {
-        Write-Host ("Verifying if {0} is installed..." -f $App)
-            if (!($CurrentVC | Select-String $App.split('+')[2].SubString(0, 4) | Select-String $App.split('-')[1])) {
-            Write-Host ("{0} not found, installing..." -f $App)
-            winget.exe install $App --force --source winget --accept-package-agreements --accept-source-agreements
-            }
+    Foreach ($App in $json.MSVCRuntime) {
+    Write-Host ("Verifying if {0} is installed..." -f $App)
+        if (!($CurrentVC | Select-String $App.split('+')[2].SubString(0, 4) | Select-String $App.split('-')[1])) {
+        Write-Host ("{0} not found, installing..." -f $App)
+        winget.exe install $App --force --source winget --accept-package-agreements --accept-source-agreements
         }
+    }
     Foreach ($App in $json.Apps) {
     Write-Host ("Verifying if {0} is installed..." -f $App)
     winget.exe list --id $App --accept-source-agreements | Out-Null
@@ -46,9 +46,6 @@ if (!(Get-AppxPackage -Name Microsoft.Winget.Source)) {
     }
 }
 Stop-Transcript
-
-
-
 
 
 

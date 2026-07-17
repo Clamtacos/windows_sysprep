6f0790817ae2a4744e66d1d00949af1170d0d5a7
clamtacos <5408736+Clamtacos@users.noreply.github.com>
2026-07-17 05:16:39 -0400

    Update windows_sysprep.ps1

--- windows_sysprep.ps1
+++ windows_sysprep.ps1
@@ -18,7 +18,7 @@ Start-Transcript $ENV:TEMP\windows_sysprep.log
 Set-ExecutionPolicy Bypass -Force:$True -Confirm:$False -ErrorAction SilentlyContinue
 Set-Variable -Name 'ConfirmPreference' -Value 'None' -Scope Global
 $ProgressPreference = 'SilentlyContinue'
-$json = Get-Content -Path "($PSScriptRoot)\windows_sysprep.json" | ConvertFrom-Json
+$json = Get-Content -Path ".\\windows_sysprep.json" | ConvertFrom-Json
 
 if (!(Get-AppxPackage -Name Microsoft.Winget.Source)) {
     Write-Host ("Winget not found, installing...")
